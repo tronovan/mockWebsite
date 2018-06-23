87f9e7230f4786e2e82b7dc38140f593efc6a007
Donovan Venuto <tronovan@gmail.com>
2018-06-23 09:43:03 -0500

    setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
 
+});
+
+gulp.task('watch', function(){
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+
+    watch('./index.html', function(){
+        browserSync.reload();
+    });
+
+    watch('./style.css', function(){
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', function(){
+    return gulp.src('./style.css').pipe(browserSync.stream());
 });
